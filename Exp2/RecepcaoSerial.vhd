896f4698143ef2f53cbefc863e14f7a4140e4af2
DanZambuja <daniel.azambuja@usp.br>
2017-09-11 14:52:12 -0300

    unidade_controle

---
+++ Exp2/RecepcaoSerial.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity RecepcaoSerial is
+  port (
+    clock     : in std_logic;
+    dados     : out std_logic
+  );
+end RecepcaoSerial;
+
+architecture hierarquico of RecepcaoSerial is
+
+begin
+
+
+end hierarquico;
---
+++ Exp2/fluxo_dados.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity fluxo_dados is
+  port ();
+end fluxo_dados;
+
+architecture combinatorio of fluxo_dados is
+
+begin
+
+
+end combinatorio;

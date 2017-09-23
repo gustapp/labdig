7089f9867c86764123d1778e8b7cb6c2ef99a63b
Gustavo Polleti <gustavo.polleti@gmail.com>
2017-09-23 12:36:36 -0300

    CTS logic

--- exp4/transmissão/circuito_transmissao.vhd
+++ exp4/transmissão/circuito_transmissao.vhd
@@ -6,6 +6,7 @@ entity circuito_transmissao is
 		  enviar			 	 : in  std_logic;
 		  reset		    	 : in  std_logic;
 		  clock		    	 : in  std_logic;
+		  CTS					 : in  std_logic;
 		  dado_serial   	 : in  std_logic;
 		  envioOk	    	 : out std_logic;
 		  DTR			       : out std_logic;
@@ -27,9 +28,11 @@ architecture circuito_transmissao_Arch of circuito_transmissao is
 			  enviar			 	 	: in  std_logic;
 			  reset		    	 	: in  std_logic;
 			  clock		    	 	: in  std_logic;
+			  CTS						: in	std_logic;
 			  DTR			       	: out std_logic;
 			  RTS		       	 	: out std_logic;
 			  enable_transmissao : out std_logic;
+			  envioOk				: out std_logic;
 			  s_estado				: out std_logic_vector(1 downto 0));
 	end component;
 
@@ -37,9 +40,7 @@ signal s_enable_transmissao : std_logic;
 	
 begin 
 	
-	k1 : unidade_controle_transmissao port map (liga, enviar, reset, clock, DTR, RTS, s_enable_transmissao, d_estado);
+	k1 : unidade_controle_transmissao port map (liga, enviar, reset, clock, CTS, DTR, RTS, s_enable_transmissao, envioOk, d_estado);
 	k2 : fluxo_de_dados_transmissao   port map (dado_serial, s_enable_transmissao, TD);
-	
-	envioOk <= s_enable_transmissao;
 
 end circuito_transmissao_Arch;
